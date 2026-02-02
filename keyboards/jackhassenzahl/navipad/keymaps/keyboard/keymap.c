233ec5618233597f1ece0de46a11a6396676bbda
Jack Hassenzahl <127689226+jackhassenzahl@users.noreply.github.com>
2026-02-02 12:50:33 -0500

    Update keymap for navipad with new key functions

--- keyboards/jackhassenzahl/navipad/keymaps/keyboard/keymap.c
+++ keyboards/jackhassenzahl/navipad/keymaps/keyboard/keymap.c
@@ -6,7 +6,7 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [0] = LAYOUT(
-        KC_S,    KC_D,    KC_L,    KC_C,    MO(1),
+        KC_S,    KC_D,    KC_L,    KC_C,    LT(1,KC_ESC),
         KC_H,    KC_E,    KC_T,    KC_A,    KC_O,
         KC_W,    KC_U,    KC_N,    KC_M,    KC_I,
         MO(2),   TG(3),   KC_SPC,  KC_DOT,  SC_SENT
@@ -21,13 +21,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [2] = LAYOUT(
         KC_K,    KC_P,    KC_B,    KC_V,    KC_CAPS,
-        KC_Y,    KC_R,    KC_F,    KC_G,    KC_ESC,
+        KC_Y,    KC_R,    KC_F,    KC_G,    KC_SLSH,
         KC_J,    KC_X,    KC_Q,    KC_Z,    KC_QUOT,
         KC_TRNS, TG(4),   KC_BSPC, KC_COMM, KC_TRNS
     ),
 
     [3] = LAYOUT(
-        KC_PEQL, KC_7,    KC_8,    KC_9,    MO(1),
+        KC_PEQL, KC_7,    KC_8,    KC_9,    LT(1,KC_ESC),
         KC_DOT,  KC_4,    KC_5,    KC_6,    KC_PSLS,
         SC_SENT, KC_1,    KC_2,    KC_3,    KC_PAST,
         KC_NO,   TG(3),   KC_0,    KC_PPLS, KC_PMNS
